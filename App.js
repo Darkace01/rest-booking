a8661ded81fc378ff107f0b5b52477154b88f65c
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-09-13 15:45:07 +0100

    redux set up

--- App.js
+++ App.js
@@ -1,47 +1,14 @@
-import { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from './src/screens/SplashScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  LOADING_SCREEN,
-  LOGIN_SCREEN,
-  REGISTER_SCREEN,
-  SPLASH_SCREEN,
-} from './src/constants/screenRoutes';
-import Loading from './src/components/Loading';
-import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './src/navigations/navigator';
-
-const Stack = createNativeStackNavigator();
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
-  const [viewedOnboarding, setViewedOnboarding] = useState(false);
-
-  const checkOnboarding = async () => {
-    try {
-      const onboardingCompleted = await AsyncStorage.getItem(
-        '@viewedOnboarding'
-      );
-      if (onboardingCompleted !== null) {
-        setViewedOnboarding(true);
-      }
-    } catch (err) {
-      console.log('Error @checkOnboarding', err);
-    } finally {
-      setLoading(false);
-    }
-  };
-  useEffect(() => {
-    checkOnboarding();
-  }, []);
   return (
-    <>
+    <Provider store={store}>
       <StatusBar style='auto' />
       <AppNavigation />
-    </>
+    </Provider>
   );
 }
