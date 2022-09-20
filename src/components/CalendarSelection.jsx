763e1fc43217c84e2dccf78d49d442506c18de8e
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-09-20 17:56:27 +0100

    render custom arrows

--- src/components/CalendarSelection.jsx
+++ src/components/CalendarSelection.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Calendar } from 'react-native-calendars';
 import { useState } from 'react';
 import { colors, fonts } from '../constants/globalStyles';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const CalendarSelection = () => {
   var today = new Date();
@@ -20,12 +21,39 @@ const CalendarSelection = () => {
       },
     });
   };
+
+  const ArrowLeft = () => {
+    return (
+      <MaterialIcons
+        name='keyboard-arrow-left'
+        size={24}
+        color={colors.primary}
+      />
+    );
+  };
+  const ArrowRight = () => {
+    return (
+      <MaterialIcons
+        name='keyboard-arrow-right'
+        size={24}
+        color={colors.primary}
+      />
+    );
+  };
+
+  const renderArrow = (direction) => {
+    if (direction === 'left') {
+      return <ArrowLeft />;
+    }
+    return <ArrowRight />;
+  };
   return (
     <View>
       <Calendar
         minDate={today.toISOString()}
         markedDates={markedDate}
         onDayPress={handleDateSelection}
+        renderArrow={renderArrow}
         theme={{
           backgroundColor: '#ffffff',
           calendarBackground: '#ffffff',
